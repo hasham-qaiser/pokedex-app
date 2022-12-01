bd0d0ecf27b88d7130be1eda870639a8fa79a15d
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2022-11-30 20:55:39 -0800

    finished pokedex (1-150)
    updated HTML and JS to display pokemons in a card style

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
-// as soon as website loads
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 150;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
 
-//https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-window.addEventListener("load", () => {
-  const randomNumber = randomIntFromInterval(1, 898);
-  //everything here will only run when the page loads
-  //make GET request
-  const baseURL = "https://pokeapi.co/api/v2/pokemon";
-  fetch(`${baseURL}/${randomNumber}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const pokemonDiv = document.getElementById("pokemon");
+const main_types = Object.keys(colors);
 
-      const name = data.name;
-      const header = document.createElement("h1");
-      header.innerText = name;
-      pokemonDiv.append(header);
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
 
-      const shinySprite = data.sprites.front_shiny;
-      //create img element
-      const img = document.createElement("img");
-      //set img src as shiny sprite
-      img.src = shinySprite;
-      //add img element to the div id= 'pokemon'
-      pokemonDiv.append(img);
-    });
-});
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, "0");
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `<div class="img-container">
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+    alt="">
+</div>
+<div class="info">
+  <span class="number">#${id}</span>
+  <h3 class="name">${name}</h3>
+  <small class="type">Type: <span>${type}</span> </small>
+</div>
+  `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
